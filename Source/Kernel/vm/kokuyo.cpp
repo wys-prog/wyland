89a262b169508d6e276002d78da1e8d2600a3608
Wys <wys-workshop@outlook.fr>
2025-01-26 21:09:15 +0100

    IM JUST CODING AND GIT SAYIN' ME TE WRITE A F*CKING COMMIT

--- Source/Kernel/vm/kokuyo.cpp
+++ Source/Kernel/vm/kokuyo.cpp
@@ -1,85 +1,99 @@
 // コクウキョウ - Kokūkyō
 //            - Wys
 //            - Wyland
-// This version of Kokuyo is modified. 
+// This version of Kokuyo is modified.
 // It built specialy for the Wyland project.
 // This code is free, and open source.
 
-#include <vector>
-#include <sstream>
-#include <string>
-#include <functional>
-#include <unordered_map>
 #include <cstdint>
+#include <functional>
+#include <sstream>
+#include <stdfloat>
+#include <string>
 #include <typeindex>
 #include <typeinfo>
+#include <unordered_map>
+#include <vector>
 
-class core {
+#define BYTE  1
+#define WORD  2
+#define DWORD 4
+#define QWORD 8
+
+template <typename T>
+T cast_array(uint8_t *buff, uint64_t len) {
+  T tmp = T(); 
+  int bwi = (sizeof(T) - 1) * 8; 
+  
+  for (uint64_t i = 0; i < len && i < sizeof(T); ++i) {
+    tmp |= (static_cast<T>(buff[i]) << bwi); 
+    bwi -= 8;
+  }
+
+  return tmp;
+}
+
+class bad_register    : public std::exception {};
+class bad_instruction : public std::exception {};
+class stack_overflow  : public std::exception {};
+
+
+class kokuyo {
 private:
-  // Programs are dynamic. They can change size.
+  uint64_t regs[32]; // 32 registers 
+  size_t   ip;
+  uint64_t stack[4096];
+
   std::vector<uint8_t> program;
 
-  // Registers are not in memory. Memory didn't exists.
-  std::unordered_map<uint8_t, uint64_t> r64 = {
-    {'a', 0x0000000000000000},
-    {'b', 0x0000000000000000},
-    {'c', 0x0000000000000000},
-    {'d', 0x0000000000000000},
-    {'@', 0x0000000000000000}, // Istruction Pointer (ip)
-    {'%', 0x0000000000000000}, // Stack Pointer
-  };
+  uint8_t read8() { return program[ip++]; }
 
-  std::unordered_map<uint8_t, uint32_t> r32 = {
-    {'a', 0x00000000},
-    {'b', 0x00000000},
-    {'c', 0x00000000},
-    {'d', 0x00000000},
-  };
-
-  std::unordered_map<uint8_t, uint16_t> r16 = {
-    {'a', 0x0000},
-    {'b', 0x0000},
-    {'c', 0x0000},
-    {'d', 0x0000},
-  };
-
-  std::unordered_map<uint8_t, uint8_t> r8 = {
-    {'a', 0x00},
-    {'b', 0x00},
-    {'c', 0x00},
-    {'d', 0x00},
-  };
-
-  std::unordered_map<uint8_t, std::type_index> opT = {
-    {0x00, typeid(uint8_t)}, 
-    {0x01, typeid(uint16_t)}, 
-    {0x02, typeid(uint32_t)}, 
-    {0x03, typeid(uint64_t)},
-  };
-
-  template <typename T>
-  T cast_array(uint8_t *buff, uint64_t len) {
-    T tmp = T(); 
-    int bwi = (sizeof(T) - 1) * 8; 
-
-    for (uint64_t i = 0; i < len && i < sizeof(T); ++i) {
-      tmp |= (static_cast<T>(buff[i]) << bwi); 
-      bwi -= 8;
-    }
-
-    return tmp;
+  uint64_t read64() {
+    uint8_t buff[8] = {read8(), read8(), read8(), read8(), read8(), read8(), read8(), read8(), };
+    return cast_array<uint64_t>(buff, 8);
   }
 
-  template <typename T>
-  T read() {
-    uint8_t *buff = new uint8_t[sizeof(T)];
-    for (int i = 0; i < sizeof(T); i++) {
-      buff[i] = program[r64['@']++];
-    }
+  std::unordered_map<uint8_t, std::function<void()>> ftable = {
+    {0x00, [this]() {}}, // NOP
+    {0x01, [this]() {
+        uint8_t r = read8();
+        uint64_t data = read64();
 
-    return cast_array<T>(buff, sizeof(T));
-  }
+        if (r < 32) regs[r] = data;
+        else throw bad_register();
+      }
+    }, 
+    {0x02, [this]() {
+        regs[read8()] = regs[read8()];
+      }
+    }, 
+    {0x03, [this]() {
+        regs[read8()] += regs[read8()];
+      }
+    },
+    {0x04, [this]() {
+        regs[read8()] -= regs[read8()];
+      }
+    }, 
+    {0x05, [this]() {
+        regs[read8()] *= regs[read8()];
+      }
+    },
+    {0x06, [this]() {
+        regs[read8()] /= regs[read8()];
+      }
+    },
+    {0x07, [this]() {
+        uint8_t a = read8();
+        uint8_t b = read8();
+        regs[a] = regs[a] % regs[b];
+      }
+    },
+    {0x08, [this]() {}
+      
+    }
+  };
 
 public:
-
 };
+
