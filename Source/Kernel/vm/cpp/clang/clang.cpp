0df16a5571f45e53555a5c295d4d60d66f5110cb
Hüseyin ÖZTÜRK <wys@Huseyins-MacBook-Pro.local>
2025-02-06 15:41:41 +0100

    coding.

--- Source/Kernel/vm/cpp/clang/clang.cpp
+++ Source/Kernel/vm/cpp/clang/clang.cpp
@@ -27,6 +27,17 @@ private:
     {"syscall", SYSCALL}, {"halt", HALT},
   };
 
+    std::unordered_map<std::string, uint8_t> registers = {
+    {"r0", 0}, {"r1", 1}, {"r2", 2}, {"r3", 3},
+    {"r4", 4}, {"r5", 5}, {"r6", 6}, {"r7", 7},
+    {"r8", 8}, {"r9", 9}, {"r10", 10}, {"r11", 11},
+    {"r12", 12}, {"r13", 13}, {"r14", 14}, {"r15", 15},
+    {"r16", 16}, {"r17", 17}, {"r18", 18}, {"r19", 19},
+    {"r20", 20}, {"r21", 21}, {"r22", 22}, {"r23", 23},
+    {"r24", 24}, {"r25", 25}, {"r26", 26}, {"r27", 27},
+    {"r28", 28}, {"r29", 29}, {"r30", 30}, {"r31", 31},
+  };
+
   std::istream &input;
   std::ostream &output;
 
@@ -42,8 +53,21 @@ private:
           output.write(buff, 1);
         } else if (word.starts_with(';')) {
           std::getline(iss, line); // Skip the line.
-        } else if (false) {
-          
+        } else if (word.starts_with('$')) {
+          if (registers.find(lower(word.substr(1))) != registers.end()) {
+            char buff[1] = {registers[lower(word.substr(1))]};
+            output.write(buff, 1);
+          }
+        } else if (lower(word) == "@int") {
+          int64_t i;
+          iss >> i;
+          if (iss.fail()) throw std::invalid_argument("Not a number");
+          output.write(reinterpret_cast<const char*>(i), sizeof(i));
+        } else if (lower(word) == "@uint") {
+          uint64_t i;
+          iss >> i;
+          if (iss.fail()) throw std::invalid_argument("Not a number");
+          output.write(reinterpret_cast<const char*>(i), sizeof(i));
         } else { // Unknown symbol. Represents data.
           char *buff = new char[word.size()];
           output.write(buff, word.size());
