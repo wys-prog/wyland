96911265bbc13dd5baae0fdd1034e8f8c562ca65
Hüseyin ÖZTÜRK <wys-workshop@outlook.fr>
2025-02-13 15:52:52 +0100

    nothing.

--- Source/Kernel/vm/cpp/wyland.cpp
+++ Source/Kernel/vm/cpp/wyland.cpp
@@ -26,11 +26,21 @@ namespace wyland {
   enum class wobject_type {
     res,          // Files in .res
     logfile,      // Files in .log
-    cachedir,     // Files in .cache
+    cache,        // Files in .cache
     vm,           // Files in .vm
     binfile,      // Files in .bin
     functionlib,  // Files in .fnlib
     systemfile,   // Files in .wyls
+  };
+
+  std::unordered_map<std::string, wobject_type> extentions2type = {
+    {"res", wobject_type::res}, 
+    {"log", wobject_type::logfile}, 
+    {"cache", wobject_type::cache}, 
+    {"vm", wobject_type::vm}, 
+    {"bin", wobject_type::binfile}, 
+    {"fnlib", wobject_type::functionlib}, 
+    {"wyls", wobject_type::systemfile}, 
   };
 
   class wobject {
@@ -98,9 +108,9 @@ namespace wyland {
 
     std::fstream &get_stream() { return stream; }
 
-    ~wobject() {
-      if (stream.is_open()) stream.close();
-    }
+    wobject() = default;
+    wobject(const stdfs::path &_p) : path(_p) {}
+    wobject(const stdfs::path &_p, wobject_type _t) : path(_p), type(_t) {}
   };
 
   class VMHandle {
@@ -122,7 +132,7 @@ namespace wyland {
   class Wyland {
   private:
     stdfs::path workspace;
-    std::unordered_map<std::string, wobject> objects;
+    std::unordered_map<std::string, wobject&> objects;
     std::unordered_map<std::string, VMHandle> handles;
 
     void init() {
@@ -133,7 +143,7 @@ namespace wyland {
           ".wyland/vm/", ".wyland/logs/", ".wyland/cache/", 
           ".wyland/shell/", ".wyland/shell/bin", ".wyland/shell/templates/", 
           ".wyland/logs/wyland.log",
-        }) {
+         }) {
           stdfs::path path = stdfs::absolute(p);
           stdfs::create_directories(path);
           out.log("Created: ", path);
@@ -143,9 +153,29 @@ namespace wyland {
       }
     }
 
-  public:
-    
+    void launch() {
+      for (const auto &dir : stdfs::recursive_directory_iterator(workspace)) {
+        if (dir.is_regular_file()) {
+          stdfs::path path = dir.path();
+          if (extentions2type.find(path.extension()) != extentions2type.end()) {
+            objects[path.filename()] = wobject(path, extentions2type[path.extension()]);
+            out.log("Added file: ", path);
+          }
+        }
+      }
+    }
+
+    void load() {
+      out.log("Loading ", objects.size(), " objects...");
+      for (const auto &object : objects) 
+        object.second.loadself();
+    }
+
+  public:  
+
     Wyland(int argc, char *const argv[]) {
+      workspace = "./.wyland/";
+
       std::ofstream wyland_log;
       wyland_log.open(".wyland/logs/wyland.log");
 
@@ -153,6 +183,13 @@ namespace wyland {
         out.add_stream("std:log", wyland_log);
       } else std::cerr << "?: [e]: Failed to open log file." << std::endl;
       
+      if (!stdfs::exists(workspace)) {
+        out.error("Wyland is not initalized in this directory.");
+        out.error(workspace, ": No such directory.");
+        out.log("Aborting.");
+        WylandExit = -1;
+        return;
+      }
     }
 
     int WylandExit = 0;
