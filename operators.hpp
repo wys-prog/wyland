8214d45f53501e00e28a791340602e83dfe6e967
Hüseyin ÖZTÜRK <wys-workshop@outlook.fr>
2025-02-21 19:39:53 +0100

    coding.

--- operators.hpp
+++ operators.hpp
@@ -1,24 +1,67 @@
 #pragma once
 
+#include <cstdint>
+#include <utility>
+#include "bytes.hpp"
+
 namespace op {
-  class opsc {
-  public:
-    template <typename TyA, typename TyB>
-    void mov(TyA &a, TyB &b) { a = b; };
+  template <typename T>
+  std::function<void(T&, T&)> mov = [](T &b, T &a) { a = b; };
 
-    template <typename TyA, typename TyB>
-    void add(TyA &a, TyB &b) { a += b; };
+  template <typename T>
+  std::function<void(T&, T&)> add = [](T &b, T &a) { a += b; };
 
-    template <typename TyA, typename TyB>
-    void sub(TyA &a, TyB &b) { a -= b; };
+  template <typename T>
+  std::function<void(T&, T&)> sub = [](T &b, T &a) { a -= b; };
 
-    template <typename TyA, typename TyB>
-    void mul(TyA &a, TyB &b) { a *= b; };
-    
-    template <typename TyA, typename TyB>
-    void div(TyA &a, TyB &b) { a /= b; };
+  template <typename T>
+  std::function<void(T&, T&)> mul = [](T &b, T &a) { a *= b; };
 
-    template <typename TyA, typename TyB>
-    void mod(TyA &a, TyB &b) { a = a % b; };
+  template <typename T>
+  std::function<void(T&, T&)> div = [](T &b, T &a) { a /= b; };
+
+  template <typename T>
+  std::function<void(T&, T&)> mod = [](T &b, T &a) { a = a % b; };
+
+  class anyone {
+  private:          
+    uint64_t buff;
+
+  public:           
+    template <typename T>
+    anyone &operator=(const T &__t) {
+      buff = uint64_t(__t);
+      return *this;
+    }
+
+    template <typename T>
+    anyone &operator+=(const T &__t) {
+      buff += uint64_t(__t);
+      return *this;
+    }
+
+    template <typename T>
+    anyone &operator-=(const T &__t) {
+      buff -= uint64_t(__t);
+      return *this;
+    }
+
+    template <typename T>
+    anyone &operator*=(const T &__t) {
+      buff *= uint64_t(__t);
+      return *this;
+    }
+
+    template <typename T>
+    anyone &operator/=(const T &__t) {
+      buff /= uint64_t(__t);
+      return *this;
+    }
+
+    template <typename T>
+    T operator%(const T &__t) {
+      return buff % uint64_t(__t);
+    }
   };
+
 } // namespace op
